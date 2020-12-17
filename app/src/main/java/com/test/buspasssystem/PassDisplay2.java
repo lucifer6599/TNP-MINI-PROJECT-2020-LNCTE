0a0ec057a2e3c3378884cee37c15a95d6239b5b2
Aniket Sinha <akshaysinha6599@gmail.com>
2020-12-18 05:22:56 +0530

    Final Update

---
+++ app/src/main/java/com/test/buspasssystem/PassDisplay2.java
@@ -0,0 +1,14 @@
+package com.test.buspasssystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PassDisplay2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pass_display2);
+    }
+}
--- app/src/main/java/com/test/buspasssystem/Student.java
+++ app/src/main/java/com/test/buspasssystem/Student.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -23,7 +24,8 @@ public class Student extends AppCompatActivity {
         all.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(Student.this,DisplayAll.class));
+               // startActivity(new Intent(Student.this,DisplayAll.class));
+                Toast.makeText(getApplicationContext(),"Student Cant Perform This activity",Toast.LENGTH_SHORT).show();
             }
         });
         app.setOnClickListener(new View.OnClickListener() {
